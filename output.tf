6b50197aa67de7b72118c42909793fe62d3d4c60
Ankesh Kedarnath <ankesh.kedarnath@hach.com>
2020-09-26 22:11:41 +0530

    changing output variable

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
-output "vnet_id" {
+output "az_vnet_id" {
   description = "The id of the newly created vNet"
   value = azurerm_virtual_network.az_vnet.id
 }
 
-output "vnet_name" {
+output "az_vnet_name" {
   description = "The Name of the newly created vNet"
   value = azurerm_virtual_network.az_vnet.name
 }
 
-output "vnet_location" {
+output "az_vnet_location" {
   description = "The location of the newly created vNet"
   value = azurerm_virtual_network.az_vnet.location
 }
 
-output "vnet_address_space" {
+output "az_vnet_address_space" {
   description = "The address space of the newly created vNet"
   value = azurerm_virtual_network.az_vnet.address_space
 }
